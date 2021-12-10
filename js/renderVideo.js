9e30d63a781b3073f4b0b49c2beab15d02979749
Maksym <maksymRozhko7@gmail.com>
2021-12-10 02:09:38 +0200

    work with API, render first screen movie block, the most popular movie of the week.

---
+++ js/renderVideo.js
@@ -0,0 +1,24 @@
+import { getTriends } from './services.js';
+
+const filmWeek = document.querySelector('.film-week');
+
+const firstRender = ({ vote_average, backdrop_path, name, original_name, title, original_title }) => {
+    filmWeek.innerHTML = `
+        <div class="container film-week__container" data-rating="${vote_average}">
+            <div class="film-week__poster-wrapper">
+                <img class="film-week__poster" src="https://www.themoviedb.org/t/p/w1920_and_h800_multi_faces${backdrop_path}" alt="постер ${name}">
+                <p class="film-week__title_origin">${original_name || original_title}</p>
+            </div>
+            <h2 class="film-week__title">${name || title}</h2>
+            <a target="_blank" class="film-week__watch-trailer tube" href="https://youtu.be/V0hagz_8L3M" aria-label="смотреть трейлер"></a>
+        </div>
+    `;
+};
+
+const renderVideo = async() => {
+    const data = await getTriends();
+
+    firstRender(data.results[0]);
+};
+
+export default renderVideo;
---
+++ js/services.js
@@ -0,0 +1,19 @@
+const API_KEY = '9c1c8c3d6f7f720a49339025b46447ab';
+const BASE_URL = 'https://api.themoviedb.org/3/';
+const LANGUAGE = '&language=ru-RU';
+
+// trending/all/day?api_key=<<api_key>>
+
+const getData = url => fetch (url)
+    .then(response => {
+        if(response.ok) {
+            return response.json()
+        }
+        throw `Что-то пошло не так, ошибка ${response.status}`;
+    })
+    .catch(err => console.error(err));
+
+export const getTriends = async (type = 'all', period = 'week', page = 1) => {
+    const url = `${BASE_URL}trending/${type}/${period}?api_key=${API_KEY}${LANGUAGE}&page=${page}`;
+    return await getData(url);
+};
